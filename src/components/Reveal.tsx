36d32c2103cc6b368bc2cdcbc06e3c50b0342556
Brendan Lawless <brendanlawless@hotmail.com>
2023-10-05 13:24:21 +0100

    Reveal styles

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -4,23 +4,27 @@ export default function Reveal({ currentGameArr, setChoice, setScore }: any) {
 
     const [playerChoice, playerLoses, playerWins] = [...currentGameArr]
     const [housePick, setHousePick] = useState<number>(0)
+    const [reveal, setReveal] = useState<boolean>(false)
 
     useEffect(() => {
-        console.log(housePick)
-        if (!housePick) {
-            setHousePick(Math.floor(Math.random() * 3))
-        }
-        if (resultFunc() === 'win') {
-            setScore((prevScore: number) => prevScore += 1)
-        }
+        setTimeout(() => {
+            if (!housePick) {
+                setHousePick(Math.floor(Math.random() * 3))
+                setReveal(true)
+            }
+            if (resultFunc() === 'win') {
+                setScore((prevScore: number) => prevScore += 1)
+            }
+        }, 1000);
     }, [housePick])
-
     return (
-        <div className="revealContainer">
-            <div className="revealContainer_result">
-                <p>{`you ${resultFunc()}`}</p>
-                <button onClick={() => setChoice(undefined)}>play again</button>
-            </div>
+        <div className={reveal ? "revealContainer revealed" : "revealContainer"}>
+            {
+                <div className="revealContainer_result">
+                    <p className="font-700">{`you ${resultFunc()}`}</p>
+                    <button className="font-700" onClick={() => setChoice(undefined)}>play again</button>
+                </div >
+            }
             <div className="revealContainer_player">
                 <div className={` button revealButton button_${playerChoice}`} >
                     <div>
@@ -30,14 +34,19 @@ export default function Reveal({ currentGameArr, setChoice, setScore }: any) {
                 <h2 className="font-600">you picked</h2>
             </div>
             <div className="revealContainer_house">
-                <div className={` button revealButton button_${currentGameArr[housePick]}`} >
+                <div
+                    className={reveal
+                        ? ` button revealButton button_${currentGameArr[housePick]}`
+                        : 'button revealButton revealButton_blank'} >
                     <div>
-                        <img src={`/assets/images/icon-${currentGameArr[housePick]}.svg`} alt={currentGameArr[housePick]} />
+                        {reveal &&
+                            <img src={`/assets/images/icon-${currentGameArr[housePick]}.svg`} alt={currentGameArr[housePick]} />
+                        }
                     </div>
                 </div>
                 <h2 className="font-600">the house picked</h2>
             </div>
-        </div>
+        </div >
     )
 
     function resultFunc(): string {
